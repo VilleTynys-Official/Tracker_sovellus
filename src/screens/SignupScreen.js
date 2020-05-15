ca167bb5806e8d69a87309b1b819b590d8ec1800
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-15 15:45:46 +0300

    Lisätty navigointi Signin screeniin.

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-const SignupScreen = () => {
-    return (
-        <Text style={StyleSheet.create({ fontSize: 50})} >SignupScreen</Text>
+const SignupScreen = ({ navigation }) => {
+    return(
+        <View>
+            <Text style={StyleSheet.create({ fontSize: 50})} >SignupScreen</Text>
+            <Button title='Go to signin' onPress={() => navigation.navigate('Signin')}/>
+        </View>
     )
-};
+    };
 
 
 const styles = StyleSheet.create({});

cd0840863909aaf4d7ca4230d37a4542ad724f76
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-15 15:37:19 +0300

    Navigation stitchup.

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+
 import { StyleSheet, Text, View } from 'react-native';
 import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
 
-export default function App() {
-  return (
-    <TrackDetailScreen></TrackDetailScreen>)
-};
+//STICHING TOGETHER THE NAVIGATORS
+//switch on ylin navigaatio täs sovelluksessa.
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
+});
+
+export default createAppContainer(switchNavigator);

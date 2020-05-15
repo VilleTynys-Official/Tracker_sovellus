6dd3ad66d67d110e411d2a30ab8b6bf9efdccedb
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-15 15:19:40 +0300

    paketit installoitu ja testattu yhdellä screenillä että toimii.

---
+++ src/screens/AccountScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const AccountScreen = () => {
+    return (
+        <Text style={StyleSheet.create({ fontSize: 50})} >AccountScreen</Text>
+    )
+};
+
+
+const styles = StyleSheet.create({});
+
+
+export default AccountScreen;

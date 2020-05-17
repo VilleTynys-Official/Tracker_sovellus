0d2abfc484deec46b0538793c276c2b43b9d5a85
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-17 14:56:39 +0300

    kansion nimi muutettu?

---
+++ track-server/src/models/Track.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+
+//child schema, jossa on karttapisteet.
+const pointSchema = new mongoose.Schema({
+    timestamp: Number,
+    coords: {
+        latitude: Number,
+        longitude: Number,
+        altitude: Number,
+        accuracy: Number,
+        heading: Number,
+        speed: Number
+    }
+});
+
+
+//schema trackille.
+const trackSchema =new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+
+    },
+    name: {
+        type: String,
+        default: '',
+    },
+    locations: [pointSchema]
+});
+
+mongoose.model('Track', trackSchema);
+

ef8a70e9adf2a9dbb6c840bd048038a4b96ef74a
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-30 22:43:21 +0300

    AuthContextin hyödyntäminen.

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { Context} from '../context/AuthContext';
 
 const SigninScreen = () => {
+    const  {state, signin} = useContext(Context)
+
     return (
         <Text style={StyleSheet.create({ fontSize: 50})} >SigninScreen</Text>
     )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {View, StyleSheet } from 'react-native';
 import {Text, Input, Button} from 'react-native-elements'; //helpottaa stylingiä..?
 import Spacer from '../components/Spacer';
-
-
-
+import { Context as AuthContext} from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+
+    const {state, signup} = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -33,7 +33,9 @@ const SignupScreen = ({ navigation }) => {
                         secureTextEntry
                     />
             <Spacer>
-                <Button title="Sign Up"></Button>
+                <Button title="Sign Up"
+                        onPress={ () => signup({ email, password})}
+                        ></Button>
             </Spacer>
         </View>
     )

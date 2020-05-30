39fab268287e9d29cd928260ca7a71050b12f3b8
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-30 22:03:09 +0300

    axios setup.

---
+++ src/api/tracker.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+//axios:lla apin kanssa keskustelu.
+//ngorkilla serverin hostaus:
+
+//1 aja servu local hostiin
+//2 avaa ngrokilla localhost portti jossa on servu.
+//3 päivitä 8h välein!!!!!!!
+
+export default axios.create({
+    baseURL: 'http://11ae5174138b.ngrok.io/'
+})

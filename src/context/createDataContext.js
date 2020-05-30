df82ec61c4b0b6c01435395bea5617204743ff28
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-30 17:59:06 +0300

    Context ja provider.

---
+++ src/context/createDataContext.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from "react";
+
+
+export default (reducer, actions, defaultValue) =>{
+    const Context = React.createContext();
+
+
+    //dispatchia kutsutaan jollain actionilla ja päivittää tilan..
+    //Provider on apukomponentti
+    const Provider = ({ children}) => {
+        const [state, dispatch] = useReducer(reducer, defaultValue);
+
+        const boundActions = {};
+        for (let key in actions) {
+            boundActions[key] = actions[key](dispatch);
+        }
+
+        return (
+            <Context.Provider value={{ state, ...boundActions}}>
+                {children}
+            </Context.Provider>
+        )
+    };
+
+
+    return { Context, Provider};
+};
+

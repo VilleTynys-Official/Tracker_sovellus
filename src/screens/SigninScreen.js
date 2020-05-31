fdf76fcf61cdba479ae6083f7870c7bc42c5b540
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-31 19:40:31 +0300

    Perusvirittely.

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,17 +1,51 @@
-import React, { useContext } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import { Context} from '../context/AuthContext';
+import React, {useContext} from 'react';
+import {View, StyleSheet } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import {Context} from '../context/AuthContext';
 
 const SigninScreen = () => {
-    const  {state, signin} = useContext(Context)
+    const {state, signin} = useContext(Context);
 
-    return (
-        <Text style={StyleSheet.create({ fontSize: 50})} >SigninScreen</Text>
+
+
+
+    return(
+        <View style={styles.container}>
+            <AuthForm 
+                headerText='Sign in to your Account'
+                errorMessage= {state.errorMessage}
+                submitButtonText= 'Sign In'
+                onSubmit= {signin}    // sama kuin:   ({ email, password}) => signup({ email, password})
+            />
+            <NavLink
+                text= "Don't have an account. Sign Up instead!"
+                routeName= "Signup"
+            />
+        </View>
     )
 };
 
+//kumotaan navigationin perusasetus.
+SigninScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    };
+};
 
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 150
+    },
+    link: {
+        color: 'blue',
+    }
+        
+    
+});
 
 
 export default SigninScreen;

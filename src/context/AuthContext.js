02bf442ca17661ec35a80904f7a23bc7ff61de45
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-05-31 11:22:51 +0300

    Implisiittinen return, Tokenin tallennus AsyncStoragella puhelimen cacheen.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,33 +1,35 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
 
 
-
+//dispatch suorittaa tämän reducerin (eli muuttaa statea actionin (type ja payload) mukaisesti):
 const authReducer = (state, action) => {
     switch (action.type) {
-
-        //jos tulee error nii stateen heitetään päällimmäiseksi payload.
         case 'add_error':
-            return {...state, errorMessage: action.payload}
+            return {...state, errorMessage: action.payload};  //jos tulee error nii stateen heitetään päällimmäiseksi payload.
+        case 'signup':
+            return { errorMessage: '', token: action.payload }; //nollaa errorMessagen ja asettaa tokenin stateen.
         default:
             return state;
     }
 };
 
+//..USING IMPLICIT RETURN FUNCTIONS (Returnia ei tarvii kirjoittaa)
+
 //rakennetaan actionit joita kutsutaan
-//dispatch vastaanottaa "globaalin" kutsun.
-const signup =  (dispatch) => {
-    return async ({ email, password }) => {
+const signup =  (dispatch) => async ({ email, password }) => {
         //make api request to sign up with that email and password
         //jos sign up onnistui, muutetaan tilaa
         //jos singup epäonnistui
         try {
             const response = await trackerApi.post('/signup', { email, password});
-            console.log(response.data);
+            await AsyncStorage.setItem( 'token', response.data.token) //tallennetaan token puhelimen muistiin.
+            dispatch({ type: 'signup', payload: response.data.token})
         } catch (err){
-            dispatch({ type: 'add_error', payload: 'Something went wrong with signup'})
-        };
+            dispatch({ type: 'add_error', payload: 'Something went wrong with signup'
+            });
     };
 };
 
@@ -52,5 +54,5 @@ const signout = dispatch =>{
 export const { Provider, Context} = createDataContext(
     authReducer,
     { signin, signout, signup },
-    { isSignedIn: false, errorMessage: ''}
-)
+    { token: null, errorMessage: ''}
+);

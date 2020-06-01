99b0d9df00336bec3f56f65c98f13f061fc1492b
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-01 20:09:52 +0300

    ResolveAuth lisätty ekaksi Screeniksi.

--- App.js
+++ App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-
-
-import { StyleSheet, Text, View } from 'react-native';
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
+import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { setNavigator} from './src/navigationRef';
 
 //STICHING TOGETHER THE NAVIGATORS
 //switch on ylin navigaatio täs sovelluksessa.
 const switchNavigator = createSwitchNavigator({
+  ResolveAuth: ResolveAuthScreen,
+
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
     Signin: SigninScreen

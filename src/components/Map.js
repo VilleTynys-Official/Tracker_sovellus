541e1611dd423a0be68d91563acd8f007f63b99a
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-01 20:56:26 +0300

    PolyLinellä piirtäminen setup.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {Text, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Polyline} from 'react-native-maps'; 
 
 
 const Map = () => {
+    let points = [];
+    for (let i = 0; i < 20; i++){
+        points.push({
+            latitude: 37.3233 + i * 0.001,
+            longitude: -122.03121 + i * 0.001
+        });
+    };
+
+
+
     return <MapView
                 style={style.map}
                 initialRegion={{
@@ -12,7 +22,12 @@ const Map = () => {
                     latitudeDelta: 0.01,
                     longitudeDelta: 0.01
                 }}
-            />;
+            >
+                <Polyline coordinates={points}
+                            strokeWidth={30} 
+                            strokeColor='pink' />
+            </MapView>
+             
 
 }
 

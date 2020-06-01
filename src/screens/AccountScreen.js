5b80c06ed0d29e8e3a43eaf907e95850cf0aa122
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-01 20:26:39 +0300

    Ulos kirjautuminen, joka poistaa cachesta JWT:n.

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {Button} from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import {Context as AuthContext} from '../context/AuthContext';
 
 const AccountScreen = () => {
+    const {signout} = useContext(AuthContext);
+
     return (
-        <Text style={StyleSheet.create({ fontSize: 50})} >AccountScreen</Text>
-    )
+        <>
+            <Text style={StyleSheet.create({ fontSize: 50})} >AccountScreen</Text>
+            <Button title='Sign Out' onPress={signout}/>
+        </>
+    
+        )
 };
 
 

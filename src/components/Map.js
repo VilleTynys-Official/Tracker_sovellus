22e958215e7d2cbe1972467e672e0d23c1134b6a
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-01 20:42:11 +0300

    Map ja createTrack scaffold.

---
+++ src/components/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const Map = () => {
+    return <Text>i am a map</Text>
+
+}
+
+const style= StyleSheet.create({
+
+
+});
+
+export default Map;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { StyleSheet} from 'react-native';
+import { Text} from 'react-native-elements';
+import { SafeAreaView} from 'react-navigation';
+import Map from '../components/Map';
+
 
 const TrackCreateScreen = () => {
     return (
-        <Text style={StyleSheet.create({ fontSize: 50})} >TrackCreateScreen</Text>
-    )
+        <SafeAreaView forceInset= {{top: 'always'}}>
+            <Text h2>TrackCreateScreen</Text>
+            <Map />
+        </SafeAreaView>
+    );
 };
 
 

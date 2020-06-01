811c006874e5558b037ad678aedd6fd6a55b389c
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-01 20:31:51 +0300

    LIsätty SafeView jotta heading on paremmin aseteltu.

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,15 +3,16 @@ import {View, Text, StyleSheet} from 'react-native';
 import {Button} from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import {Context as AuthContext} from '../context/AuthContext';
+import { SafeAreaView } from 'react-navigation';
 
 const AccountScreen = () => {
     const {signout} = useContext(AuthContext);
 
     return (
-        <>
+        <SafeAreaView forceInset={{ top: 'always'}}>
             <Text style={StyleSheet.create({ fontSize: 50})} >AccountScreen</Text>
             <Button title='Sign Out' onPress={signout}/>
-        </>
+        </SafeAreaView>
     
         )
 };

4feb1eaeca749bc82a6776ad5024c2643a80b245
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-01 20:48:18 +0300

    Karttaan lisätty koordinaatti propsit.

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,12 +4,22 @@ import MapView from 'react-native-maps';
 
 
 const Map = () => {
-    return <Text>i am a map</Text>
+    return <MapView
+                style={style.map}
+                initialRegion={{
+                    latitude: 37.33233,
+                    longitude: -122.03121,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01
+                }}
+            />;
 
 }
 
 const style= StyleSheet.create({
-
+    map: {
+        height: 300
+    }
 
 });
 

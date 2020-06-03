a89ae7c81d3cb680d7c7f6306fbbe3cffacfa2df
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-03 10:22:28 +0300

    Lisätty addLocation provideriin.

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -2,22 +2,23 @@ import createDataContext from './createDataContext';
 
 const locationReducer = (state, action) =>{
     switch (action.type){
-    default:    
-        return state;
+        case 'add_current_location':
+            return{ ...state, currentLocation: action.payload}
+        default:    
+            return state;
     }
 };
 
 
-
-
-const startRecording = dispatch = () =>{};
-const stopRecording = dispatch = () => {};
-const addLocation = dispatch = () => {};
+const startRecording = dispatch => () =>{};
+const stopRecording = dispatch => () => {};
+const addLocation = dispatch => (location) => {
+    dispatch({ type: 'add_current_location', payload: location })
+};
 
 
 export const{ Context, Provider } = createDataContext(
     locationReducer,
     { startRecording, stopRecording, addLocation},
-    { recording: false, locations: [], currentLocatioin: null
-    }
+    { recording: false, locations: [], currentLocation: null }
 )

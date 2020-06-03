1d015d30633e8a122173217648df4f0f2a366e24
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-03 13:09:43 +0300

    Piirretään ympyrä käyttäjän sijaintiin. Lisäksi tehty Button keskitykselle, jota ei ole viritetty käyttöön.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,45 @@
-import React, {useContext} from 'react';
-import {Text, StyleSheet, ActivityIndicator} from 'react-native';
-import MapView, {Polyline} from 'react-native-maps';
+import React, {useContext, useState} from 'react';
+import {Text, Button, StyleSheet, ActivityIndicator} from 'react-native';
+import MapView, {Polyline, Circle } from 'react-native-maps';
 import {Context as LocationContext} from '../context/LocationContext';
 
 
 
 const Map = () => {
     const { state: {currentLocation} } = useContext(LocationContext);
+    const {isFollowing, setFollowing} = useState(true);
     //console.log(currentLocation.coords);
 
     if(!currentLocation){
         return <ActivityIndicator size= 'large' style={{ marginTop: 200 }}/> //näyttää spinnerin kun ei sijaintia..
     }
 
-    return <MapView
+    return (
+    <>
+        <MapView
                 style={style.map}
                 initialRegion={{
                     ...currentLocation.coords,
                     latitudeDelta: 0.01, //Deltas are for "zooming"
                     longitudeDelta: 0.01
                 }}
-                region={{ //keskittää kartan aina kun päivittyy.
-                    ...currentLocation.coords,          
-                    latitudeDelta: 0.01,
-                    longitudeDelta: 0.01 }}
 
+                // region={{ //keskittää kartan aina kun päivittyy.
+                //     ...currentLocation.coords,          
+                //     latitudeDelta: 0.01,
+                //     longitudeDelta: 0.01 }}
 
             >
+                <Circle
+                    center={currentLocation.coords}
+                    radius={100}
+                    strokeColor="rgba(158,158,255, 1.0)"
+                    fillColor="rgba(158,158,255, 0.3)"
+                />
             </MapView>
-             
+        
+            <Button title={isFollowing? 'Follow location': 'Unfollow location'}/>
+        </>)
 
 }
 

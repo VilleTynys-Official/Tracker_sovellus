2668512efb79b4cb8afbeb79124110f5a159093a
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-13 22:15:04 +0300

    url korjaus.

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -4,10 +4,10 @@ import axios from 'axios';
 //ngorkilla serverin hostaus:
 
 //1 aja servu local hostiin: npm run dev
-//2 avaa ngrokilla localhost portti jossa on servu: ngrok http 3000
+//2 avaa ngrokilla localhost portti jossa on servu: ngrok http 3001
 //3 päivitä 8h välein!!!!!!!
 
 export default axios.create({
-    baseURL: 'http://b798367e20af.ngrok.io '
+    baseURL: 'http://b798367e20af.ngrok.io'
 })
 

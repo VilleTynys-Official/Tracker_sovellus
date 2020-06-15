11a6b5b2fede646d7e335eed9af23ba6ceb30903
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-15 18:15:46 +0300

    Otettu käyttöön kartan keskitys aina kun uusi sijainti päivittyy.

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,13 +6,16 @@ import {Context as LocationContext} from '../context/LocationContext';
 
 
 const Map = () => {
-    const { state: {currentLocation} } = useContext(LocationContext);
+    const { state: {currentLocation, locations} } = useContext(LocationContext);
     //console.log(currentLocation.coords);
 
     //jos ei sijaintia, näyttää latausindikaattorin
     if(!currentLocation){
         return <ActivityIndicator size= 'large' style={{ marginTop: 200 }}/> //näyttää spinnerin kun ei sijaintia..
     }
+
+    // console.log(locations)
+
 
     return (
     <>
@@ -24,10 +27,10 @@ const Map = () => {
                     longitudeDelta: 0.01
                 }}
 
-                // region={{ //keskittää kartan aina kun päivittyy.
-                //     ...currentLocation.coords,          
-                //     latitudeDelta: 0.01,
-                //     longitudeDelta: 0.01 }}
+                region={{ //keskittää kartan aina kun päivittyy.
+                    ...currentLocation.coords,          
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01 }}
 
             >
                 <Circle
@@ -36,6 +39,7 @@ const Map = () => {
                     strokeColor="rgba(158,158,255, 1.0)"
                     fillColor="rgba(158,158,255, 0.3)"
                 />
+                <Polyline coordinates={locations.map(loc => loc.coords)}/>
             </MapView>
         
         </>)

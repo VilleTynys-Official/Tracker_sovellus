e02a100b7ffa2713d81f1da0a8d5f9c7ca39367c
Ville Tynys <villetynys@Ville-MacBook-Pro-4.local>
2020-06-15 19:46:15 +0300

    korjattu nimet.

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -11,11 +11,13 @@ const trackReducer = (state, action) => {
 };
 
 
-const fetchTracks =dispatch => () =>{};
-const createTracks = dispatch => () =>{};
+const fetchTrack =dispatch => () =>{};
+const createTrack = dispatch => (name, locations) =>{
+    console.log('apiin luodaan track jonka tiedot ovat:', name, locations.length);
+};
 
 export const { Provider, Context} = createDataContext(
     trackReducer,
-    { fetchTracks, createTracks},
+    { fetchTrack, createTrack},
     []
 );
